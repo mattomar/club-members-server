ef46a6c8c05ebc7dae97623cf15fc34810eebe56
mattomar <moddathero@gmail.com>
2025-03-20 14:48:53 +0700

    fixed the db url

--- config/database.js
+++ config/database.js
@@ -1,21 +1,15 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Required for Neon
     },
-  }
-);
+  },
+});
 
 // Test connection
 sequelize
